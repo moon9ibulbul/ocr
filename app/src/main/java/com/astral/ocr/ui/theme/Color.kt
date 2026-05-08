dd238e44ebca3dc5c4e44e39d5eadce00849e567
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2026-05-08 15:24:59 +0700

    Add color definitions for theme

---
+++ app/src/main/java/com/astral/ocr/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.astral.ocr.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MidnightBlue = Color(0xFF0F172A)
+val CosmicPurple = Color(0xFF5B21B6)
+val StellarPink = Color(0xFFE879F9)
+val AuroraCyan = Color(0xFF22D3EE)
