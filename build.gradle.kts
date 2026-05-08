06439a25ab239243e5c35099ae09a8d713d280a7
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2026-05-08 15:33:52 +0700

    Add initial Gradle configuration for Android project

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.24")
+    }
+}
